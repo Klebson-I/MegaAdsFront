0a81b0c6c423748f3d0feef233a9013a5e86e94a
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-05-13 12:09:12 +0200

    Add leaflet to project and deploy map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import './Map.css';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import  'leaflet/dist/leaflet.css';
 
 export const Map = () => {
     return <div className="map">
-        ...
+        <MapContainer center={[50,18]} zoom={13}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[50,18]}>
+                <Popup>
+
+                </Popup>
+            </Marker>
+        </MapContainer>
     </div>
 }
